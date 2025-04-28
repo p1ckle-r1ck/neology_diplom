3f3f6ef280546e0f203e741a1450cab64873d0b0
Marsel <shelbygt500ss28@gmail.com>
2025-04-28 11:20:29 +0300

    fmt

--- 00_s3_storage/00_s3_storage.tf
+++ 00_s3_storage/00_s3_storage.tf
@@ -19,20 +19,20 @@ resource "yandex_iam_service_account" "service_account_s3" {
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "service_account_editor" {
-  folder_id = var.cloud.folder_id
-  role      = "storage.editor"
-  member    = "serviceAccount:${yandex_iam_service_account.service_account_s3.id}"
-  depends_on = [ yandex_iam_service_account.service_account_s3 ]
+  folder_id  = var.cloud.folder_id
+  role       = "storage.editor"
+  member     = "serviceAccount:${yandex_iam_service_account.service_account_s3.id}"
+  depends_on = [yandex_iam_service_account.service_account_s3]
 }
 
 resource "yandex_iam_service_account_static_access_key" "service_account_key" {
   service_account_id = yandex_iam_service_account.service_account_s3.id
   description        = "static access key for object storage"
-  depends_on = [ yandex_resourcemanager_folder_iam_member.service_account_editor ]
+  depends_on         = [yandex_resourcemanager_folder_iam_member.service_account_editor]
 }
 
 resource "yandex_storage_bucket" "object_storage" {
-  depends_on = [ yandex_iam_service_account_static_access_key.service_account_key ]
+  depends_on = [yandex_iam_service_account_static_access_key.service_account_key]
   bucket     = "marsel.valiev"
   access_key = yandex_iam_service_account_static_access_key.service_account_key.access_key
   secret_key = yandex_iam_service_account_static_access_key.service_account_key.secret_key
