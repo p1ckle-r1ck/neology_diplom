d3b786684373c068c181a17d3e702164d622eb25
Marsel <shelbygt500ss28@gmail.com>
2025-05-20 21:56:50 +0300

    add README

--- 01_cloud/70_jenkins.tf
+++ 01_cloud/70_jenkins.tf
@@ -8,3 +8,12 @@ resource "null_resource" "jenkins_deployer" {
   depends_on = [ null_resource.atlantis_deployer ]
   
 }
+
+resource "null_resource" "jenkins_roles" {
+  provisioner "local-exec" {
+    command = <<EOT
+        kubectl apply -f ../06_jenkins/role.yml --kubeconfig ./kubeconfig/kubeconfig
+    EOT
+  }
+  depends_on = [ null_resource.jenkins_deployer ]  
+}
